3a68afdfc0fe5f7fafbd1b60152f3441d2fa7a6a
Niklas Tommy Färeborg <Fareborg@hotmail.com>
2021-10-26 15:26:39 +0000

    deployment commit

--- brewery/settings.py
+++ brewery/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["brewery.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["brewery-events.herokuapp.com", "localhost"]
 
 
 # Application definition
